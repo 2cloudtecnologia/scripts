458fbdc95e86c87b44895409971c7539c44d6fed
Abner Klug <abner.klug@2cloud.com.br>
2021-08-03 14:44:35 -0300

    Ajuste visual no checar_recursos.sh

--- checar_recursos.sh
+++ checar_recursos.sh
@@ -2,5 +2,10 @@
 CPU=$(cat /proc/cpuinfo | grep processor | wc -l)
 MEMORIA=$(free -h | awk 'NR==2{print $2}')
 
+echo ""
+echo "--- Script para verificar recursos em Linux ---"
+echo ""
 echo "Quantidade de CPU: $CPU"
 echo "Quantidade de memória: $MEMORIA"
+echo ""
+echo ""
