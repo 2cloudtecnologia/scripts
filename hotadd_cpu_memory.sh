e45fb6a960ba3071d9d47b6e54af8b7045df3dca
Abner Klug <abner.klug@2cloud.com.br>
2021-08-03 14:09:05 -0300

    Traduzido

--- hotadd_cpu_memory.sh
+++ hotadd_cpu_memory.sh
@@ -2,28 +2,31 @@
 for CPU_DIR in /sys/devices/system/cpu/cpu[0-9]*
 do
     CPU=${CPU_DIR##*/}
-    echo "Found cpu: '${CPU_DIR}' ..."
+    echo "Encontrado CPU: '${CPU_DIR}' ..."
     CPU_STATE_FILE="${CPU_DIR}/online"
     if [ -f "${CPU_STATE_FILE}" ]; then
         if grep -qx 1 "${CPU_STATE_FILE}"; then
-            echo -e "\t${CPU} already online"
+            echo -e "\t${CPU} já está online"
         else
-            echo -e "\t${CPU} is new cpu, onlining cpu ..."
+            echo -e "\t${CPU} nova CPU, colocando como online ..."
             echo 1 > "${CPU_STATE_FILE}"
         fi
     else
-        echo -e "\t${CPU} already configured prior to hot-add"
+        echo -e "\t${CPU} já configurado para hot-add"
     fi
 done
 
 # Bring all new Memory online
 for RAM in $(grep line /sys/devices/system/memory/*/state)
 do
-    echo "Found ram: ${RAM} ..."
+    echo "Encontrada RAM: ${RAM} ..."
     if [[ "${RAM}" == *":offline" ]]; then
-        echo "Bringing online"
+        echo "Colocando como online"
         echo $RAM | sed "s/:offline$//"|sed "s/^/echo online > /"|source /dev/stdin
     else
-        echo "Already online"
+        echo "Já está online"
     fi
 done
+echo ""
+echo ""
+echo "--------> Tudo pronto! <--------"
